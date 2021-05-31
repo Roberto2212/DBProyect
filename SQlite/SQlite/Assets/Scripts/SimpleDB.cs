a7f459c23d54ebe356b421434c2204ff2eed685e
Enkrai <enkrai22@gmail.com>
2021-06-01 00:34:35 +0200

    Json Done
    Se ha introducido json a una escena nueva.

--- SQlite/SQlite/Assets/Scripts/SimpleDB.cs
+++ SQlite/SQlite/Assets/Scripts/SimpleDB.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 using Mono.Data.Sqlite;
 using System.Data;
 using System;
+using UnityEngine.UI;
 
-
-public class NewBehaviourScript : MonoBehaviour
+public class SimpleDB : MonoBehaviour
 {
 
-    private string dbName = "URI=file:inventory.db";
+    public Text texto;
+    private string dbName = "URI=file:INVENTORYDB.db";
 
     // Start is called before the first frame update
     void Start()
     {
+        CreateDB();
         AddItem("Iron Sword", 10, 15);
         AddItem("Golden Sword",20, 30);
+         AddItem("Platinum Axe", 30, 40);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKey(KeyCode.S))
         CreateDB();
 
-        if(Input.GetKeyDown(KeyCode.L))
+        if(Input.GetKey(KeyCode.L))
         ShowItems();
     }
 
@@ -36,6 +40,7 @@ public class NewBehaviourScript : MonoBehaviour
 
             using (var command = connection.CreateCommand())  // Se utiliza una variable para insertar un comando en SQLite
             {
+                Debug.Log("Saving in to: " + dbName);
                 command.CommandText = "CREATE TABLE IF NOT EXISTS inventory (ItemName VARCHAR(20), STA INT, STR INT);";    //Crea una tabla si no existe una previa, llamada productsTable que contiene en su interior un VARCHAR de 20 caracteres ASCII y un float para determinar el precio.
                 command.ExecuteNonQuery();  // Ejecuta el comando en SQL en la query de la DB. 
             }
@@ -60,6 +65,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void ShowItems()
     {
+        
          using (var connection = new SqliteConnection(dbName))  // Se utiliza una variable para crear una conexión con la base de datos a través de la .dll de Mono.Data.Sqlite
         {
             connection.Open();  // Abrimos la conexión
@@ -70,10 +76,11 @@ public class NewBehaviourScript : MonoBehaviour
                using (IDataReader reader = command.ExecuteReader())
                {
                    while (reader.Read())
-                   {
-                       Debug.Log("Nombre: " + reader["ItemName"] + "\tSTA: " + reader["STA"] + "\tSTR: " + reader["STR"]);
-                   }
+                   
+                      texto.text = ("Nombre: " + reader["ItemName"] + "\tSTA: " + reader["STA"] + "\tSTR: " + reader["STR"] + "\n");
+                   
                    reader.Close();
+                   
                }
             }
             connection.Close(); // Cierra la conexión
